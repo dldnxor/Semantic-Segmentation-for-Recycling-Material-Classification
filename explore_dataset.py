f5dd914042074cc29884aa1d8665c30048170d85
Alias26 <khmjuhee979@gmail.com>
2022-12-21 16:15:29 +0900

    add: new dataset type fiftyone

--- explore_dataset.py
+++ explore_dataset.py
@@ -1,29 +1,31 @@
 import fiftyone as fo
 import json
 
-# from .mmsegmentation.mmseg.datasets.boost import BOOSTDataset
+train_name = "train"
+val_name = "val"
+data_path = "../data/"
+train_labels_path = "../data/train.json"
+val_labels_path = "../data/val.json"
 
-name = "my-dataset"
-data_path = "../data/mmseg/images/train/"
-labels_path = "../data/mmseg/annotations/train/"
 
-# dataset_type = BOOSTDataset
-
-# Create the dataset
-dataset = fo.Dataset.from_dir(
+train_dataset = fo.Dataset.from_dir(
     data_path=data_path,
-    labels_path=labels_path,
-    dataset_type=fo.types.ImageSegmentationDirectory,
-    # dataset_type=dataset_type,
-    name=name,
+    labels_path=train_labels_path,
+    dataset_type=fo.types.COCODetectionDataset,
+    label_types=["segmentations"],
+    name=train_name,
+)
+
+val_dataset = fo.Dataset.from_dir(
+    data_path=data_path,
+    labels_path=val_labels_path,
+    dataset_type=fo.types.COCODetectionDataset,
+    label_types=["segmentations"],
+    name=val_name,
 )
 
 
-# View summary info about the dataset
-print(dataset)
-
-# Print the first few samples in the dataset
-print(dataset.head())
-
-session = fo.launch_app(dataset)
-session.wait()
+if __name__ == "__main__":
+    session = fo.launch_app(train_dataset)
+    session.dataset = val_dataset
+    session.wait()
